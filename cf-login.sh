f50d31d2b6c119632a28a694149a277ae3bda087
Super-Turbo-Engineer <128454215+super-turbo-engineer@users.noreply.github.com>
2025-06-13 16:38:49 +1200

    change to 127.0.0.1

--- cf-login.sh
+++ cf-login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# For use in headless environment (i.e. containers) and remaps 0.0.0.0 to localhost for the callback url
-wrangler login --browser=false --callback-host=0.0.0.0 --callback-port=8976 | stdbuf -oL sed 's/0\.0\.0\.0/localhost/g'
+# For use in headless environment (i.e. containers) and remaps 127.0.0.1 to localhost for the callback url
+wrangler login --browser=false --callback-host=127.0.0.1 --callback-port=8976 | stdbuf -oL sed 's/127\.0\.0\.1/localhost/g'
